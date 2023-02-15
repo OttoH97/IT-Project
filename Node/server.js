1cefa28e42d6e4fe029ecf72c92940aa46eb36b6
JuusoKas <82642652+JuusoKas@users.noreply.github.com>
2023-02-15 10:43:49 +0200

    test

--- Node/server.js
+++ Node/server.js
@@ -1,6 +1,38 @@
+var express = require(express);
+
+var app = express();
+
 var http = require('http');
 
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('Hello World!');
-}).listen(8080);
+var myAPIKey= 'dc55e8bbc6b73dbb17c5ecf360a0aeb1'
+
+
+var options = {
+
+    host:'weldcude.ky.local',
+
+    port: 80,
+
+    path:'/api/v4/Welds',
+
+    method:'GET',
+
+    headers: {
+        "api_key": myAPIKey
+      }
+
+};
+
+http.request(options, function(res){
+    var body ='';
+
+    res.on('data', function(chunk){
+        body += chunk;
+    });
+
+    res.on('end', function(){
+        var values = JSON.parse(body);
+        console.log(values);
+    });
+
+}).end();
