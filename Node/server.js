3cd5fd0e231f8238a7bd429d1eebb8c074a61135
JuusoKas <82642652+JuusoKas@users.noreply.github.com>
2023-02-27 14:57:03 +0200

    asd

--- Node/server.js
+++ Node/server.js
@@ -1,48 +1,28 @@
+// app.js
 var express = require('express');
-var xmlparser = require('express-xml-bodyparser');
-var http = require('http');
-var parseString = require('xml2js').parseString; // import xml2js library for parsing XML
-
 var app = express();
+const fetch = require('node-fetch');
 
+// other Express middleware and configurations
 var myAPIKey = 'dc55e8bbc6b73dbb17c5ecf360a0aeb1';
 
-app.use(xmlparser());
 
-app.get('/welds', (req, res) => {
-  const options = {
-    host: 'weldcube.ky.local',
-    port: 80,
-    path: '/api/v4/Welds',
-    method: 'GET',
-    headers: {
-      'api_key': myAPIKey,
-      'Accept': 'application/xml'
-    }
+
+app.get('/welds', async (req, res) => {
+  const url = 'http://weldcube.ky.local/api/v4/Welds';
+  const headers = {
+    'api_key': myAPIKey,
+    'Accept': 'application/json'
   };
 
-  // Make API request
-  http.request(options, function(response) {
-    var str = '';
-    response.on('data', function(chunk) {
-      str += chunk;
-    });
-
-    response.on('end', function() {
-      // Parse XML data using xml2js library
-      parseString(str, function(err, result) {
-        if (err) {
-          console.error(err);
-          res.status(500).send('Error parsing XML data');
-        } else {
-          // Send parsed XML data to React front-end
-          res.send(result);
-        }
-      });
-    });
-  }).end();
+  try {
+    const response = await fetch(url, { headers });
+    const data = await response.json();
+    res.send(data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send(error);
+  }
 });
 
-app.listen(3000, () => {
-  console.log('Server listening on port 3000');
-});
+app.listen(3000, () => console.log('Server running on port 3000'));
