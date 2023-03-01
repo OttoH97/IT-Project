995a294dc075a2d923dbd9d4089e9e93a83a3677
JuusoKas <82642652+JuusoKas@users.noreply.github.com>
2023-03-01 14:34:45 +0200

    server merged

--- Node/server.js
+++ Node/server.js
@@ -1,38 +1,104 @@
-var express = require(express);
-
+var express = require('express');
+require('dotenv').config()
+var nodeMailer = require('nodemailer');
 var app = express();
+let cors = require('cors');
+app.use(cors());
+const axios = require('axios');
 
 var http = require('http');
 
-var myAPIKey= 'dc55e8bbc6b73dbb17c5ecf360a0aeb1'
 
 
-var options = {
 
-    host:'weldcube.ky.local',
 
-    port: 80,
 
-    path:'/api/v4/Welds',
 
-    method:'GET',
 
-    headers: {
-        "api_key": myAPIKey
-      }
-
+//Mailserverin luonti
+const transporter = nodeMailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: '587',
+    secure: false,
+    tls: {
+        ciphers: "SSLv3",
+        rejectUnauthorized: false,
+        },
+        //Autentikointiin tarvittavat tiedot .env tiedostossa. 
+    auth:{
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
+    }
+});
+//Mailoptions.to määritellään JSON-listan mappauksessa. 
+const Mailoptions  = {
+    from: 'WeldMailer123@gmail.com',
+    subject: 'testataan',
+    text: "tämä olla viesti, jee", 
 };
-
-http.request(options, function(res){
-    var body ='';
-
-    res.on('data', function(chunk){
-        body += chunk;
+//Tämä kohta korvataan erillisellä Json tiedostolla/objektilla
+const recipients = {
+    'John Doe': 'johndoe@example.com',
+    'Jane Doe': 'janedoe@example.com',
+    'Bob Smith': 'bobsmith@example.com'
+  };
+//for -loopissa käydään läpi jokainen objektissa oleva sähköposti
+  for (const [name, email] of Object.entries(recipients)) {
+    Mailoptions.to = email;
+    Mailoptions.text = `Hello ${name},\n\n${Mailoptions.text}`;
+    
+    transporter.sendMail(Mailoptions, (error, info) => {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log(`Viesti lähetetty: ${name} (${email}): ` + info.response);
+      }
     });
+  }
 
-    res.on('end', function(){
-        var values = body;
-        console.log(values);
+  app.get('/welds', async (req, res) => {
+    const url = 'http://weldcube.ky.local/api/v4/Welds';
+    const headers = {
+      'api_key': process.env.MY_API_KEY,
+      'Accept': 'application/json'
+    };
+  
+    try {
+      const response = await fetch(url, { headers });
+      const data = await response.json();
+      res.send(data);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send(error);
+    }
+  });
+
+  //Tässä testataan POST
+
+  const apiUrl = 'http://weldcube.ky.local/api/v4/Welds/{WeldID}/ChangeState';
+
+  app.post('/welds/change-state', (req, res) => {
+    //const { WeldId, explanation, user } = req.body; // tämä ottaa frontin puolelta responsen. EI käytössä ennen kuin front valmis.
+  
+    const data = {
+      WeldId,
+      explanation : "testi",
+      user : R12_K2023
+    };
+  
+    const headers = {
+      'Content-Type': 'application/json',
+      'api_key': process.env.MY_API_KEY
+    };
+  
+    axios.post(apiUrl, data, { headers })
+    .then(response => {
+      res.json(response.data);
+    })
+    .catch(error => {
+      console.error(error);
+      res.status(500).json({ message: 'Internal server error' });
     });
-
-}).end();
+  });
+  
+  app.listen(4000, () => console.log('Server running on port 4000'));
