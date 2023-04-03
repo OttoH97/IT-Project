7d560afc2fd5184263b50da0c6a8e09625267a42
OttoH97 <82201999+OttoH97@users.noreply.github.com>
2023-04-03 14:39:01 +0300

    pagination.js

---
+++ src/components/content/pagination.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Pagination = ({ currentPage, totalPages, onPageChange }) => {
+
+  return (
+    <nav aria-label="Page navigation">
+      <ul className="pagination">
+        <li className={`page-item${currentPage === 1 ? ' disabled' : ''}`}>
+          <button className="page-link" onClick={() => onPageChange(currentPage - 1)}>
+            Previous
+          </button>
+        </li>
+        <li className={`page-item${currentPage === totalPages ? ' disabled' : ''}`}>
+          <button className="page-link" onClick={() => onPageChange(currentPage + 1)}>
+            Next
+          </button>
+        </li>
+      </ul>
+    </nav>
+
+)};
+
+export default Pagination;
